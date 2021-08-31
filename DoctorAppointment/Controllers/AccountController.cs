8ec0eb8aa730d8f73cdfd8cf502e6c2803aef3ee
OptimusV7 <manstill09@outlook.com>
2021-08-31 14:56:21 +0300

    Add project files.

---
+++ DoctorAppointment/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using DoctorAppointment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoctorAppointment.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public AccountController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
