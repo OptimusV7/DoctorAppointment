12d69eb0bbe7c8ebecc7acaa87563a2a556da8fc
OptimusV7 <manstill09@outlook.com>
2021-09-09 16:21:46 +0300

    added api endpoint to save and update appointment data

--- DoctorAppointment/Controllers/Api/AppointmentApiController.cs
+++ DoctorAppointment/Controllers/Api/AppointmentApiController.cs
@@ -1,4 +1,6 @@
-﻿using DoctorAppointment.Services;
+﻿using DoctorAppointment.Models.ViewModels;
+using DoctorAppointment.Services;
+using DoctorAppointment.Utility;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -25,9 +27,31 @@ namespace DoctorAppointment.Controllers.Api
             loginUserId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
             role = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
         }
-        public IActionResult Index()
+
+        [HttpPost]
+        [Route("SaveCalendarData")]
+        public IActionResult SaveCalendarData(AppointmentVM data)
         {
-            return View();
+            CommonResponse<int> commonResponse = new CommonResponse<int>();
+            try
+            {
+                commonResponse.status = _appointmentService.AddUpdate(data).Result;
+                if (commonResponse.status == 1)
+                {
+                    commonResponse.message = Helper.appointmentUpdated;
+                }
+                if (commonResponse.status == 2)
+                {
+                    commonResponse.message = Helper.appointmentAdded;
+                }
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.failure_code;
+            }
+
+            return Ok(commonResponse);
         }
     }
 }
