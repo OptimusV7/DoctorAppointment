8320dbe90a21c56cef3645641725f6d13e08606f
OptimusV7 <manstill09@outlook.com>
2021-09-09 12:51:32 +0300

    added show calender and doctors dropdown list viewed only by admin

--- DoctorAppointment/Controllers/AppointmentController.cs
+++ DoctorAppointment/Controllers/AppointmentController.cs
@@ -16,7 +16,7 @@ namespace DoctorAppointment.Controllers
         }
         public IActionResult Index()
         {
-            _appointmentService.GetDoctorList();
+            ViewBag.DoctorList = _appointmentService.GetDoctorList();
             return View();
         }
     }
